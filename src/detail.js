2aa1b9f18582ddf2a35e4836437ce36f55ea796a
Christian Gloor <Christian Gloor>
2017-05-19 00:39:24 +0200

    bolt mit js gelöst

--- src/detail.js
+++ src/detail.js
@@ -4,7 +4,7 @@ let template = Handlebars.templates['detail'];
 
 let detailObj = {
     title: 'blup title',
-    bla: 'bla value',
+    importance: 0,
     finDate: moment().format('YYYY-MM-DD')
 };
 
@@ -15,4 +15,17 @@ function save() {
     console.log('saving it');
     console.log(detailObj);
     console.log(template);
+}
+
+function setBoltClass(aClass, idx) {
+    let bolts = $('.detail-importance').find('i');
+    bolts.removeClass(aClass);
+    for (let i = 0; i <= idx; i++) {
+        $(bolts[i]).addClass(aClass);
+    }
+
+    if (aClass === 'active') {
+        detailObj.importance = idx;
+    }
+
 }
